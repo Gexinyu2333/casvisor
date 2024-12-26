a8baa6a4fba23b0fc0e19ef2ed92e6253d508069
Xinyu Ge <GeXinyu2333@gmail.com>
2024-12-26 14:58:54 +0800

    Update machine.go
    feat: #139 support showing VMware virtual machines in machine list and edit pages

--- object/machine.go
+++ object/machine.go
@@ -88,11 +88,10 @@ func GetMachines(owner string) ([]*Machine, error) {
 
 	for _, provider := range providers {
 		client, err2 := service.NewMachineClient(provider.Type, provider.ClientId, provider.ClientSecret, provider.Region)
-		if err2 != nil {
+		if err2 != nil || client == nil {
 			return machines, err2
 		}
-
-		clientMachines, err2 := client.GetMachines()
+		clientMachines, err2 := (*client).GetMachines()
 		if err2 != nil {
 			return machines, err2
 		}
@@ -127,18 +126,17 @@ func getMachine(owner string, name string) (*Machine, error) {
 
 	for _, provider := range providers {
 		client, err2 := service.NewMachineClient(provider.Type, provider.ClientId, provider.ClientSecret, provider.Region)
-		if err2 != nil {
+		if err2 != nil || client == nil {
 			return nil, err2
 		}
 
-		clientMachine, err2 := client.GetMachine(name)
+		clientMachine, err2 := (*client).GetMachine(name)
 		if err2 != nil {
 			return nil, err2
 		}
 		if clientMachine == nil {
 			continue
 		}
-
 		machine := getMachineFromService(owner, provider.Name, clientMachine)
 		return machine, nil
 	}
